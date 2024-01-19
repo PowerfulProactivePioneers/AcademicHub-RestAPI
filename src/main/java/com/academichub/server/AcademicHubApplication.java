d5170452b58af8436ae9262d62bdb9e7a92b6a20
Praveen-Pandian <praveenpandian04@gmail.com>
2024-01-19 21:15:15 +0530

    Scheduled

--- src/main/java/com/academichub/server/AcademicHubApplication.java
+++ src/main/java/com/academichub/server/AcademicHubApplication.java
@@ -2,8 +2,10 @@ package com.academichub.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AcademicHubApplication {
 
 	public static void main(String[] args) {
